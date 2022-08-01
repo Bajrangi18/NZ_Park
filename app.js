b78b6b5e2858c21570d9143c7accef29f1959ed0
Bharatdeep Hazarika <67242026+Bajrangi18@users.noreply.github.com>
2022-08-01 13:27:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+function menu2() {
+  document.getElementById("pingger1").style.left = "4vw";
+  document.getElementById("but1").style.filter="invert(100%)";
+  document.getElementById("but2").style.filter="invert(0%)";
+  document.getElementById("but3").style.filter="invert(100%)";
+  document.getElementById("but4").style.filter="invert(100%)";
+  document.getElementById("but5").style.filter="invert(100%)";
+  console.log("compass");
+}
+function menu3() {
+  document.getElementById("pingger1").style.left = "23vw";
+  document.getElementById("but1").style.filter="invert(100%)";
+  document.getElementById("but2").style.filter="invert(100%)";
+  document.getElementById("but3").style.filter="invert(0%)";
+  document.getElementById("but4").style.filter="invert(100%)";
+  document.getElementById("but5").style.filter="invert(100%)";
+  console.log("memo");
+
+}
+function menu1() {
+  document.getElementById("pingger1").style.left = "41.7vw";
+  document.getElementById("but1").style.filter="invert(0%)";
+  document.getElementById("but2").style.filter="invert(100%)";
+  document.getElementById("but3").style.filter="invert(100%)";
+  document.getElementById("but4").style.filter="invert(100%)";
+  document.getElementById("but5").style.filter="invert(100%)";
+  console.log("qr");
+
+}
+function menu4() {
+  document.getElementById("pingger1").style.left = "60.6vw";
+  document.getElementById("but1").style.filter="invert(100%)";
+  document.getElementById("but2").style.filter="invert(100%)";
+  document.getElementById("but3").style.filter="invert(100%)";
+  document.getElementById("but4").style.filter="invert(0%)";
+  document.getElementById("but5").style.filter="invert(100%)";
+  console.log("wallet");
+
+}
+function menu5() {
+  document.getElementById("pingger1").style.left = "79.5vw";
+  document.getElementById("but1").style.filter="invert(100%)";
+  document.getElementById("but2").style.filter="invert(100%)";
+  document.getElementById("but3").style.filter="invert(100%)";
+  document.getElementById("but4").style.filter="invert(100%)";
+  document.getElementById("but5").style.filter="invert(0%)";
+  console.log("user");
+
+}
+document.getElementById("but1").style.filter="invert(0%)";
+document.getElementById("but2").style.filter="invert(100%)";
+document.getElementById("but3").style.filter="invert(100%)";
+document.getElementById("but4").style.filter="invert(100%)";
+document.getElementById("but5").style.filter="invert(100%)";
